af1de14c6768edb06b48365e954a23974ca98630
Ben Prudence <ben@optimalcs.co.uk>
2024-10-31 21:30:11 +0000

    Adding ecs bits

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,69 +137,69 @@ module "autoscaling_sg" {
 # ALB
 ################################################################################
 
-# module "alb" {
-#   source  = "terraform-aws-modules/alb/aws"
-#   version = "~> 9.0"
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 9.0"
 
-#   name = "${local.resource_prefix}-alb"
+  name = "${local.resource_prefix}-alb"
 
-#   load_balancer_type = "application"
+  load_balancer_type = "application"
 
-#   vpc_id  = module.vpc.vpc_id
-#   subnets = module.vpc.public_subnets
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
 
-#   # Security Group
-#   security_group_ingress_rules = {
-#     all_http = {
-#       from_port   = 443
-#       to_port     = 443
-#       ip_protocol = "tcp"
-#       cidr_ipv4   = "0.0.0.0/0"
-#     }
-#   }
-#   security_group_egress_rules = {
-#     all = {
-#       ip_protocol = "-1"
-#       cidr_ipv4   = module.vpc.vpc_cidr_block
-#     }
-#   }
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = module.vpc.vpc_cidr_block
+    }
+  }
 
-#   listeners = {
-#     ex_http = {
-#       port     = 443
-#       protocol = "HTTPS"
+  # listeners = {
+  #   ex_http = {
+  #     port     = 443
+  #     protocol = "HTTPS"
 
-#       forward = {
-#         target_group_key = "ex_ecs"
-#       }
-#     }
-#   }
+  #     forward = {
+  #       target_group_key = "ex_ecs"
+  #     }
+  #   }
+  # }
 
-#   target_groups = {
-#     ex_ecs = {
-#       backend_protocol                  = "HTTP"
-#       backend_port                      = local.container_port
-#       target_type                       = "ip"
-#       deregistration_delay              = 5
-#       load_balancing_cross_zone_enabled = true
+  # target_groups = {
+  #   ex_ecs = {
+  #     backend_protocol                  = "HTTP"
+  #     backend_port                      = local.container_port
+  #     target_type                       = "ip"
+  #     deregistration_delay              = 5
+  #     load_balancing_cross_zone_enabled = true
 
-#       health_check = {
-#         enabled             = true
-#         healthy_threshold   = 5
-#         interval            = 30
-#         matcher             = "200"
-#         path                = "/"
-#         port                = "traffic-port"
-#         protocol            = "HTTP"
-#         timeout             = 5
-#         unhealthy_threshold = 2
-#       }
+  #     health_check = {
+  #       enabled             = true
+  #       healthy_threshold   = 5
+  #       interval            = 30
+  #       matcher             = "200"
+  #       path                = "/"
+  #       port                = "traffic-port"
+  #       protocol            = "HTTP"
+  #       timeout             = 5
+  #       unhealthy_threshold = 2
+  #     }
 
-#       create_attachment = false
-#     }
-#   }
+  #     create_attachment = false
+  #   }
+  # }
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}

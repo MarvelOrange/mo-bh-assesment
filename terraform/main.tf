507bb904cb1156412de79393544975ea435cdc6c
Ben Prudence <ben@optimalcs.co.uk>
2024-10-31 14:42:32 +0000

    fix chjeckov

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Create a random string so this run can be unique and not conflict with other runs
 resource "random_string" "random" {
-  length      = 4
+  length      = 5
   min_numeric = 1
   special     = false
   upper       = false

605b64c334f7120a32b82a8d877e519f7b1d3313
Ben Prudence <ben@optimalcs.co.uk>
2024-10-31 21:30:46 +0000

    Adding ecs bits

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ module "autoscaling" {
         #!/bin/bash
 
         cat <<'EOF' >> /etc/ecs/ecs.config
-        ECS_CLUSTER=${module.ecs_cluster.cluster_name}
+        ECS_CLUSTER=${module.ecs_cluster.name}
         ECS_LOGLEVEL=debug
         ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
         ECS_ENABLE_TASK_IAM_ROLE=true

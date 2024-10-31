c91f202059f7db585400eab86be5a9d18d32c0a4
Ben Prudence <ben@optimalcs.co.uk>
2024-10-31 22:59:01 +0000

    add tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,10 +2,10 @@ locals {
   resource_prefix = "mo-bh"
 
   frontend_resource_prefix = "${local.resource_prefix}-fe"
-  frontend_repo_url = aws_ecr_repository.ecr["frontend"].repository_url
+  frontend_repo_url        = aws_ecr_repository.ecr["frontend"].repository_url
 
-  backend_resource_prefix  = "${local.resource_prefix}-be"
-  backend_repo_url = aws_ecr_repository.ecr["backend"].repository_url
+  backend_resource_prefix = "${local.resource_prefix}-be"
+  backend_repo_url        = aws_ecr_repository.ecr["backend"].repository_url
 
   ssl_cert_acm_arn = data.aws_acm_certificate.mo_issued.arn
 

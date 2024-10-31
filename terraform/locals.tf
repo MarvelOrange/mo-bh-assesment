b77e5059c2a1a6898f9d6eae6bdcc41c2ee76680
Ben Prudence <ben@optimalcs.co.uk>
2024-10-31 21:28:03 +0000

    Adding ecs bits

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,12 +1,19 @@
 locals {
   resource_prefix = "mo-bh"
 
+  frontend_resource_prefix = "${local.resource_prefix}-fe"
+  backend_resource_prefix  = "${local.resource_prefix}-be"
+
+  ## VPC
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  # tags = {
-  #   Example    = local.name
-  #   GithubRepo = "terraform-aws-vpc"
-  #   GithubOrg  = "terraform-aws-modules"
-  # }
+  ## ECS
+  container_name = "ecs-sample"
+  container_port = 80
+
+  tags = {
+    GithubRepo = "mo-bh-assesment"
+    GithubOrg  = "marvelorange"
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,5 +13,193 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  # tags = local.tags
+  tags = local.tags
 }
+
+################################################################################
+# Cluster
+################################################################################
+
+module "ecs_cluster" {
+  source  = "terraform-aws-modules/ecs/aws//modules/cluster"
+  version = "5.11.4"
+
+  cluster_name = "${local.resource_prefix}-cluster"
+
+  # Capacity provider - autoscaling groups
+  default_capacity_provider_use_fargate = false
+  autoscaling_capacity_providers = {
+    # Spot instances
+    ex_2 = {
+      auto_scaling_group_arn         = module.autoscaling["ex_2"].autoscaling_group_arn
+      managed_termination_protection = "ENABLED"
+
+      managed_scaling = {
+        maximum_scaling_step_size = 15
+        minimum_scaling_step_size = 5
+        status                    = "ENABLED"
+        target_capacity           = 90
+      }
+
+      default_capacity_provider_strategy = {
+        weight = 40
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "~> 6.5"
+
+  for_each = {
+    # Spot instances
+    ex_2 = {
+      instance_type              = "t3.small"
+      use_mixed_instances_policy = false
+      mixed_instances_policy     = {}
+      user_data                  = <<-EOT
+        #!/bin/bash
+
+        cat <<'EOF' >> /etc/ecs/ecs.config
+        ECS_CLUSTER=${module.ecs_cluster.cluster_name}
+        ECS_LOGLEVEL=debug
+        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
+        ECS_ENABLE_TASK_IAM_ROLE=true
+        ECS_ENABLE_SPOT_INSTANCE_DRAINING=true
+        EOF
+      EOT
+    }
+  }
+
+  name = "${local.resource_prefix}-asg-${each.key}"
+
+  image_id      = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
+  instance_type = each.value.instance_type
+
+  security_groups                 = [module.autoscaling_sg.security_group_id]
+  user_data                       = base64encode(each.value.user_data)
+  ignore_desired_capacity_changes = true
+
+  create_iam_instance_profile = true
+  iam_role_name               = "${local.resource_prefix}-asg-role"
+  iam_role_description        = "ECS role for ${local.resource_prefix}-asg"
+  iam_role_policies = {
+    AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+    AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  vpc_zone_identifier = module.vpc.private_subnets
+  health_check_type   = "EC2"
+  min_size            = 1
+  max_size            = 1
+  desired_capacity    = 1
+
+  # https://github.com/hashicorp/terraform-provider-aws/issues/12582
+  autoscaling_group_tags = {
+    AmazonECSManaged = true
+  }
+
+  # Required for  managed_termination_protection = "ENABLED"
+  protect_from_scale_in = true
+
+  # Spot instances
+  use_mixed_instances_policy = each.value.use_mixed_instances_policy
+  mixed_instances_policy     = each.value.mixed_instances_policy
+
+  tags = local.tags
+}
+
+module "autoscaling_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.frontend_resource_prefix}-asg-sg"
+  description = "Autoscaling group security group"
+  vpc_id      = module.vpc.vpc_id
+
+  computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-80-tcp"
+      source_security_group_id = module.alb.security_group_id
+    }
+  ]
+  number_of_computed_ingress_with_source_security_group_id = 1
+
+  egress_rules = ["all-all"]
+
+  tags = local.tags
+}
+
+################################################################################
+# ALB
+################################################################################
+
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~> 9.0"
+
+#   name = "${local.resource_prefix}-alb"
+
+#   load_balancer_type = "application"
+
+#   vpc_id  = module.vpc.vpc_id
+#   subnets = module.vpc.public_subnets
+
+#   enable_deletion_protection = false
+
+#   # Security Group
+#   security_group_ingress_rules = {
+#     all_http = {
+#       from_port   = 443
+#       to_port     = 443
+#       ip_protocol = "tcp"
+#       cidr_ipv4   = "0.0.0.0/0"
+#     }
+#   }
+#   security_group_egress_rules = {
+#     all = {
+#       ip_protocol = "-1"
+#       cidr_ipv4   = module.vpc.vpc_cidr_block
+#     }
+#   }
+
+#   listeners = {
+#     ex_http = {
+#       port     = 443
+#       protocol = "HTTPS"
+
+#       forward = {
+#         target_group_key = "ex_ecs"
+#       }
+#     }
+#   }
+
+#   target_groups = {
+#     ex_ecs = {
+#       backend_protocol                  = "HTTP"
+#       backend_port                      = local.container_port
+#       target_type                       = "ip"
+#       deregistration_delay              = 5
+#       load_balancing_cross_zone_enabled = true
+
+#       health_check = {
+#         enabled             = true
+#         healthy_threshold   = 5
+#         interval            = 30
+#         matcher             = "200"
+#         path                = "/"
+#         port                = "traffic-port"
+#         protocol            = "HTTP"
+#         timeout             = 5
+#         unhealthy_threshold = 2
+#       }
+
+#       create_attachment = false
+#     }
+#   }
+
+#   tags = local.tags
+# }

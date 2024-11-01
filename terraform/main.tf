e27341607d65d1c0008df7a6c3019c27a9088309
Ben Prudence <ben@optimalcs.co.uk>
2024-11-01 12:48:23 +0000

    change path

--- terraform/main.tf
+++ terraform/main.tf
@@ -196,7 +196,7 @@ module "alb" {
           }]
           conditions = [{
             path_pattern = {
-              values = ["/backend"]
+              values = ["/backend", "/login"]
             }
           }]
         }
